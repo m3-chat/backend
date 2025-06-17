4b2b65a33a2aac6193c72d2b571eb14efbf1a3ff
myferr <contactme.myfer@protonmail.com>
2025-06-17 00:34:49 -0500

    refactor(tunnel.sh): make port configurable via command line argument

--- tunnel.sh
+++ tunnel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cloudflared tunnel --url http://localhost:2000 --config ./cloudflared.tunnel.config.yml
+cloudflared tunnel --url http://localhost:"$1" --config ./cloudflared.tunnel.config.yml

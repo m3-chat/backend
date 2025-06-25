663b90cd196ab63357faec90b0d96462c54d18ef
myferr <contactme.myfer@protonmail.com>
2025-06-24 22:02:56 -0500

    feat: add `api/models` route to list all LLMs

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from "elysia";
 import { corsPlugin } from "./plugins/cors";
 import { genRoute } from "./routes/gen";
+import { modelsRoute } from "./routes/models";
 
 const app = new Elysia()
   .use(corsPlugin)
   .use(genRoute)
+  .use(modelsRoute)
 
   .listen(Bun.env.PORT ?? 2000);
 

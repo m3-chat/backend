adc81989540830e57ecebc8505da1473e40b71f0
myferr <contactme.myfer@protonmail.com>
2025-06-24 20:36:11 -0500

    fix(plugins): import corsPlugin, not cors + remove compression import, non-existant plugin

--- app/index.ts
+++ app/index.ts
@@ -1,14 +1,11 @@
 import { Elysia } from "elysia";
-import { cors } from "./plugins/cors";
-import { compression } from "./plugins/compression";
+import { corsPlugin } from "./plugins/cors";
 import { genRoute } from "./routes/gen";
-import { statusRoute } from "./routes/status";
 
 const app = new Elysia()
-  .use(cors())
-  .use(compression())
+  .use(corsPlugin)
   .use(genRoute)
-  .use(statusRoute)
+
   .listen(Bun.env.PORT ?? 2000);
 
 console.log(

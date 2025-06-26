7736e902c125b9f35a9f159af19ae50fcfb08486
myferr <contactme.myfer@protonmail.com>
2025-06-26 15:01:46 -0500

    Refactor(scripts): Remove redundant go.mod and go.sum moves

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,6 +6,3 @@ go get golang.ngrok.com/ngrok/v2
 go get golang.ngrok.com/ngrok
 go get golang.ngrok.com/ngrok/config
 go get github.com/joho/godotenv
-
-mv ../go.mod ./ || echo "Failed to move go.sum"
-mv ../go.sum ./ || echo "Failed to move go.sum"

47213ebaf295a48713e94ab884106d5d2b5ae86b
MyferIsADev <contactme.myfer@protonmail.com>
2025-06-26 12:47:28 -0500

    fix(tunnel): use the correct binary for the localxpose CLI

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -6,7 +6,7 @@ LOGFILE="tunnel.log"
 echo "Starting LocalXpose tunnel to localhost:$PORT (logging to $LOGFILE)..."
 
 while true; do
-  lx tunnel http --to localhost:$PORT >> "$LOGFILE" 2>&1
+  loclx tunnel http --to localhost:$PORT >> "$LOGFILE" 2>&1
   if [[ $? -ne 0 ]]; then
     echo "Tunnel crashed. Restarting in 5 seconds..."
     sleep 5

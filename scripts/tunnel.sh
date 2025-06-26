7ab5a516da7a7a7570b52f4361b0edd10a1e218a
MyferIsADev <contactme.myfer@protonmail.com>
2025-06-25 22:31:16 -0500

    feat(sh): add tunnel script

---
+++ scripts/tunnel.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+PORT=3000
+LOGFILE="tunnel.log"
+
+echo "Starting LocalXpose tunnel to localhost:$PORT (logging to $LOGFILE)..."
+
+while true; do
+  lx tunnel http --to localhost:$PORT >> "$LOGFILE" 2>&1
+  if [[ $? -ne 0 ]]; then
+    echo "Tunnel crashed. Restarting in 5 seconds..."
+    sleep 5
+  else
+    echo "Tunnel exited normally."
+    break
+  fi
+done
